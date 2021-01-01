0a218609430e708651b010ede2424c8a05991655
Nathalie <nathalie.rautell@gmail.com>
2021-01-01 19:21:04 +0100

    added vuex and vue router

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <HelloWorld v-bind:msg="`Welcome to your Vue.js App ${user}`" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "../components/HelloWorld";
+import { mapState } from "vuex";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      user: "Nathalie",
+    };
+  },
+  computed: {
+    count() {
+      return store.state.user;
+    },
+  },
+  components: {
+    HelloWorld,
+  },
+};
+</script>
